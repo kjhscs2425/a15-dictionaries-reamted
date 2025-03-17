96c22b5259b286fcb14164bdc866d6305ccd9c61
reamted <nroussos2028@kehillahstudent.org>
2025-03-17 09:16:34 -0700

    extra challendge

--- main.py
+++ main.py
@@ -4,13 +4,17 @@ Everyone has access to great learning.
 Every person's unique talents are valued equally.
 Everyone takes responsibility for themselves and their community."""
 
+skip = ['E', "'", ' ', '\n', '.']
+
 letter_count = {}
 
 for letter in text:
-    if letter in letter_count:
-        letter_count[letter] += 1
-    else:
-        letter_count[letter] = 1
+        if letter in skip:
+             pass
+        elif letter in letter_count:
+            letter_count[letter] += 1
+        else:
+            letter_count[letter] = 1
 
 print(letter_count)
 
